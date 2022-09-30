0395ff5b26f7e5e3b5ea1edbc98d6b7d6c821f90
danieelfcr <danielfcab2002@gmail.com>
2022-09-30 07:16:29 -0600

    User frame designed

--- MusicPlayer/src/main/java/UserFrame.java
+++ MusicPlayer/src/main/java/UserFrame.java
@@ -25,21 +25,148 @@ public class UserFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        JTFBirthDate = new javax.swing.JTextField();
+        JTFPassword = new javax.swing.JPasswordField();
+        JBPhoto = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        JTFPhoneNumber = new javax.swing.JTextField();
+        JTFEmail = new javax.swing.JTextField();
+        JBChangeInfo = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        JBChangeInfo1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Welcome!");
+
+        jLabel2.setText("Here you can change your personal information.");
+
+        JTFPassword.setText("jPasswordField1");
+
+        JBPhoto.setText("Import photo");
+
+        jLabel5.setText("New password");
+
+        jLabel6.setText("Birth date");
+
+        jLabel7.setText("Email");
+
+        jLabel8.setText("Profile photo");
+
+        jLabel9.setText("Phone number");
+
+        JBChangeInfo.setText("DELETE ACCOUNT");
+        JBChangeInfo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBChangeInfoActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("WARNING ZONE!");
+
+        JBChangeInfo1.setText("Save changes");
+        JBChangeInfo1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBChangeInfo1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(266, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel8)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel9))
+                        .addGap(153, 153, 153)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(JTFBirthDate, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(JTFPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(JBPhoto, javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(JTFEmail, javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(JTFPhoneNumber, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(215, 215, 215))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(53, 53, 53)
+                        .addComponent(JBChangeInfo)
+                        .addGap(333, 333, 333))))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(113, 113, 113)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(459, 459, 459)
+                        .addComponent(JBChangeInfo1)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(74, 74, 74)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(JTFPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(96, 96, 96))
+                            .addComponent(JTFBirthDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(42, 42, 42)
+                        .addComponent(JTFEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
+                        .addComponent(JTFPhoneNumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(45, 45, 45)
+                        .addComponent(JBPhoto))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel2)
+                        .addGap(119, 119, 119)
+                        .addComponent(jLabel5)
+                        .addGap(59, 59, 59)
+                        .addComponent(jLabel6)
+                        .addGap(56, 56, 56)
+                        .addComponent(jLabel7)
+                        .addGap(54, 54, 54)
+                        .addComponent(jLabel9)
+                        .addGap(59, 59, 59)
+                        .addComponent(jLabel8)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
+                .addComponent(JBChangeInfo1)
+                .addGap(70, 70, 70)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(JBChangeInfo))
+                .addGap(170, 170, 170))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void JBChangeInfoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBChangeInfoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JBChangeInfoActionPerformed
+
+    private void JBChangeInfo1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBChangeInfo1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JBChangeInfo1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,5 +204,20 @@ public class UserFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JBChangeInfo;
+    private javax.swing.JButton JBChangeInfo1;
+    private javax.swing.JButton JBPhoto;
+    private javax.swing.JTextField JTFBirthDate;
+    private javax.swing.JTextField JTFEmail;
+    private javax.swing.JPasswordField JTFPassword;
+    private javax.swing.JTextField JTFPhoneNumber;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     // End of variables declaration//GEN-END:variables
 }
